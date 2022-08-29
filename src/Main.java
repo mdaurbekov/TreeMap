c2fe8d383e35c7faaada8f456cfcef6e846b670f
mdaurbekov <dreamkov@gmail.com>
2022-08-30 00:51:36 +0300

    removeIf

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
         list.add(new Person("Person_1", "aa aa aa aa", 12));
-        list.add(new Person("Person_2", "bb bb bb bb bb bb", 10));
+        list.add(new Person("Person_2", "bb bb bb bb bb bb", 20));
         list.add(new Person("Person_3", "cc cc cc cc", 13));
-        list.add(new Person("Person_4", "dd dd dd", 13));
+        list.add(new Person("Person_4", "dd dd", 23));
 
 
-        list.sort(new PersonComparator(4));
+        list.removeIf(person -> person.getAge() < 18);
         System.out.println(list);
     }
 }
